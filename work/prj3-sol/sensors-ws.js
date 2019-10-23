a6dd6031f1c51cd1ed50eb6d64642e9bab86d96f
Erv16 <epalani1@binghamton.edu>
2019-10-22 22:48:59 -0400

    Commit involves successful listening on port provided, loading of data and querying of sensor types based on query parameters. Not able to query using the self option yet.

--- work/prj3-sol/sensors-ws.js
+++ work/prj3-sol/sensors-ws.js
@@ -13,8 +13,46 @@ const SERVER_ERROR = 500;
 
 function serve(port, sensors) {
   //@TODO set up express app, routing and listen
+  const app = express();
+  app.locals.port = port;
+  app.locals.model = sensors;
+  setupRoutes(app);
+  app.listen(port,function(){
+    console.log(`listening on port ${port}`);
+  });
 }
 
 module.exports = { serve };
 
+const base = '/sensor-types';
 //@TODO routing function, handlers, utility functions
+function setupRoutes(app){
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.get(base,findSensorType(app)); 
+}
+
+function findSensorType(app){
+  return async function(req, res){
+    const q = req.query || {};
+    console.log(q);
+    try{
+      const results = await app.locals.model.findSensorTypes(q);
+      res.json(results);
+    }
+    catch(err){
+      console.err(err);
+    }
+  };
+}
+
+function errorWrap(handler){
+  return async (req, res, next) => {
+    try{
+      await handler(req, res, next);
+    }
+    catch(err){
+      next(err);
+    }
+  };
+}
